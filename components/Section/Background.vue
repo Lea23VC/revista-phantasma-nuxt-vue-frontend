c2c519b17773829b9c49ce93fb60961d08ffe5e6
Lea23VC <leandro.villalobos.dx@gmail.com>
2023-10-14 00:39:22 -0300

    test: test html in home

---
+++ components/Section/Background.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="relative h-[800px]">
+    <img
+      :src="backgroundImage"
+      class="absolute inset-0 w-full h-full object-cover"
+    />
+  </div>
+</template>
+
+<script setup>
+const backgroundImage = ref(
+  'https://static.wixstatic.com/media/9b943a_83018be20f8b4957a1e8858945ac0a43~mv2.jpg',
+);
+</script>
