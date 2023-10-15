9a0f0eebeb2f1f831e7ac277777d827210ff73bf
Lea23VC <leandro.villalobos.dx@gmail.com>
2023-10-15 01:38:30 -0300

    refactor: remove date-fns after build error

--- utils/transformation/transformDate.tsx
+++ utils/transformation/transformDate.tsx
@@ -1,10 +1,3 @@
-import { format } from "date-fns";
-import es from "date-fns/locale/es";
-import { parseISO } from "date-fns";
 export function transformDate(datetime: string) {
-  const formattedDate = format(parseISO(datetime), "dd LLL yyyy", {
-    locale: es,
-  }); // 'es' for Spanish locale
-
-  return formattedDate;
+  return datetime;
 }
