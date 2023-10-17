a8fa21db7b52453585b7c58248f80a8117bf9b56
Lea23VC <leandro.villalobos.dx@gmail.com>
2023-10-17 00:07:35 -0300

    refactor: remove padding in category blog post

--- components/Blog/Grid.vue
+++ components/Blog/Grid.vue
@@ -1,8 +1,13 @@
 <template>
-  <div class="max-w-5xl m-auto py-20 px-6 relative z-10">
-    <div class="bg-black rounded-sm p-8">
+  <div class="max-w-5xl m-auto py-20 relative z-10">
+    <div class="bg-black rounded-sm py-8">
       <div class="grid grid-cols-1 md:grid-cols-2 gap-10" v-if="posts">
-        <BlogItem v-for="(item, index) in posts" :key="index" :item="item" />
+        <BlogItem
+          v-for="(item, index) in posts"
+          :key="index"
+          :item="item"
+          height="350px"
+        />
       </div>
 
       <sloth />
