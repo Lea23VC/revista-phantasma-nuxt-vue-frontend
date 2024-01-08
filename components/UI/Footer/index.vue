2110987c62f943cabc281f6af3be52d9dcd3d7c3
Lea23VC <leandro.villalobos.dx@gmail.com>
2024-01-07 23:06:04 -0300

    refactor: add contact page

--- components/UI/Footer/index.vue
+++ components/UI/Footer/index.vue
@@ -3,53 +3,21 @@
     <div class="max-w-7xl mx-auto grid grid-cols-2 md:grid-cols-4 gap-4">
       <div class="flex flex-col space-y-2">
         <span class="font-bold text-lg">Revista Phantasma</span>
-        <p class="text-sm">Exploring the boundaries of literature and arts.</p>
+        <p class="text-sm pb-4">🌙 Literatura / Crítica / Humanidades</p>
         <div class="flex space-x-4">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            class="h-6 w-6"
+          <NuxtLink
+            to="https://www.instagram.com/revista.phantasma/"
+            target="_blank"
           >
-            <path
-              d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"
-            ></path></svg
-          ><svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            class="h-6 w-6"
+            <SVGIconIG
+          /></NuxtLink>
+
+          <NuxtLink
+            to="https://www.facebook.com/revistaphantasma"
+            target="_blank"
           >
-            <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
-            <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path>
-            <line x1="17.5" x2="17.51" y1="6.5" y2="6.5"></line></svg
-          ><svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            class="h-6 w-6"
-          >
-            <circle cx="11" cy="11" r="8"></circle>
-            <path d="m21 21-4.3-4.3"></path>
-          </svg>
+            <SVGIconFB
+          /></NuxtLink>
         </div>
       </div>
       <div class="flex flex-col space-y-2">
---
+++ pages/contacto.vue
@@ -0,0 +1,108 @@
+<template>
+  <div class="bg-black">
+    <BannersCategoryBanner
+      :backgroundImage="pageData.page?.background_image?.original_url"
+      :blur="
+        pageData.page?.background_image?.responsive_images
+          .media_library_original?.base64svg
+      "
+    />
+
+    <div
+      class="max-w-6xl px-6 pt-40 m-auto pb-40 flex flex-col md:flex-row bg-black text-white items-stretch"
+    >
+      <div class="md:w-1/2 space-y-8 flex flex-col h-full">
+        <div class="space-y-2">
+          <h1
+            class="lg:text-[80px] text-5xl text-white font-avenir font-bold pb-4"
+          >
+            Contactanos
+          </h1>
+          <p class="text-gray-400 font-avenir">
+            Envianos un mensaje y nos pondremos en contacto contigo.
+          </p>
+        </div>
+        <div class="space-y-4 flex-grow">
+          <div class="space-y-2">
+            <label
+              class="text-sm font-avenir font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 text-gray-400"
+              for="name"
+              >Nombre</label
+            ><input
+              class="flex h-10 w-full font-avenir rounded-md border border-input px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 bg-gray-800 text-white"
+              id="name"
+              placeholder="Ingrese su nombre"
+              type="text"
+              v-model="name"
+            />
+          </div>
+          <div class="space-y-2">
+            <label
+              class="text-sm font-avenir font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 text-gray-400"
+              for="email"
+              >Email</label
+            ><input
+              class="flex font-avenir h-10 w-full rounded-md border border-input px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 bg-gray-800 text-white"
+              id="email"
+              placeholder="Ingrese su email"
+              type="email"
+            />
+          </div>
+          <div class="space-y-2">
+            <label
+              class="text-sm font-avenir font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 text-gray-400"
+              for="message"
+              >Mensaje</label
+            ><textarea
+              class="flex font-avenir w-full rounded-md border border-input px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 min-h-[100px] bg-gray-800 text-white"
+              id="message"
+              placeholder="Ingrese el mensaje"
+            ></textarea>
+          </div>
+          <button
+            class="inline-flex font-avenir items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-primary/90 h-10 px-4 py-2 bg-white text-black border-white border-2 hover:!bg-black hover:text-white duration-200"
+          >
+            Enviar mensaje
+          </button>
+        </div>
+      </div>
+      <!-- <div class="md:w-1/2 flex h-full">
+        <img
+          src="/placeholder.svg"
+          alt="Contact Us"
+          class="w-full h-full object-cover self-stretch"
+          width="500"
+          height="500"
+          style="aspect-ratio: 500 / 500; object-fit: cover;"
+        />
+      </div> -->
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import PAGE_QUERY from '@/graphql/Queries/pages/getPage.query.graphql';
+
+import { Page } from '~/ts/types/page.types';
+
+const name = ref('');
+const email = ref('');
+const message = ref('');
+
+const { data: pageData, error } = await useAsyncQuery<{ page?: Page }>(
+  PAGE_QUERY,
+  {
+    slug: 'contacto',
+  },
+);
+
+useHead({
+  title: 'Contacto - Revista Phantasma',
+  meta: [
+    {
+      content:
+        'Contáctanos, espacio creativo y crítico. Colabora con Phantasma',
+    },
+  ],
+});
+</script>
